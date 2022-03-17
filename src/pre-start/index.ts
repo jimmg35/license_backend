70063880063862c1a371f6b0565a34010e92642e
MSI\Jim <jim60308@gmail.com>
2022-03-17 19:54:16 +0800

    lalala

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -15,7 +15,7 @@ import commandLineArgs from 'command-line-args'
     ])
 
     const envConfig = dotenv.config({
-        path: path.join(__dirname, `../../envConfig/production.env`),
+        path: path.join(__dirname, `../../envConfig/development.env`),
     })
 
     if (envConfig.error) {
